87d52b98f366bd961570c7ae4971239d33ac5b58
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2023-02-17 17:31:38 -0700

    Redid GPS_Test.

--- Tests/GPS_Test/GPS_Test.ino
+++ Tests/GPS_Test/GPS_Test.ino
@@ -1,83 +1,27 @@
-#include <SPI.h>
+#include <Adafruit_GPS.h>
+#include <Adafruit_PMTK.h>
+#include <NMEA_data.h>
+
 #include <SD.h>
+#include <SPI.h>
 
-File myFile;
+File test;
 
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+
+  Serial.println("Setting up SD...");
+  if(!SD.begin(4)) {
+    Serial.println("SD Setup failed.");
   }
 
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");
+  test = SD.open("tests.txt", FILE_WRITE);
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
+  test.print("Hello World!");
 
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  test.close();
 }
-
 
 void loop() {
-}
-// Ultimate GPS Using Hardware Serial
-// hardware serial port
-#define GPSSerial Serial1
-
-void setup() {
-  // baud rate
-  Serial.begin(115200);
-
-  // wait for hw serial to appear
-  while (!Serial) delay(10);
-
-  // 9600 baud is the default rate for the Ultimate GPS
-  GPSSerial.begin(9600);
-}
-
-
-void loop() {
-  if (Serial.available()) {
-    char c = Serial.read();
-    GPSSerial.write(c);
-  }
-  if (GPSSerial.available()) {
-    char c = GPSSerial.read();
-    Serial.write(c);
-  }
+  
 }
